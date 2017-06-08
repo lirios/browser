8820992f868aa9421d27b569f66c97fd9fdf6c8e
Tim Süberkrüb <dev@timsueberkrueb.io>
2017-06-08 12:00:45 +0200

    Add browser.ts file and register it with QMake

---
+++ res/translations/browser.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="115"/>
+        <source>New window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="116"/>
+        <source>New tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="117"/>
+        <source>Private mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="401"/>
+        <source>New Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="410"/>
+        <source>Private Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="419"/>
+        <source>Find in page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="429"/>
+        <source>Downloads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="438"/>
+        <source>Exit fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="438"/>
+        <source>Fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="446"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DrawerDownloadsContent</name>
+    <message>
+        <location filename="../../src/ui/drawer/content/downloads/DrawerDownloadsContent.qml" line="33"/>
+        <source>Downloads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/drawer/content/downloads/DrawerDownloadsContent.qml" line="51"/>
+        <source>No downloads for this session</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchOverlay</name>
+    <message>
+        <location filename="../../src/ui/overlay/SearchOverlay.qml" line="56"/>
+        <source>Find in page</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsContent</name>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="60"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="64"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="68"/>
+        <source>Start url</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="77"/>
+        <source>Primary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="92"/>
+        <source>Dark theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="107"/>
+        <source>Incognito</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="123"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="174"/>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="188"/>
+        <source>e.g https://example.com/?q=</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="201"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="214"/>
+        <source>Adapt to website theme colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="226"/>
+        <source>Light theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="238"/>
+        <source>Dark theme (always on)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="253"/>
+        <source>Dark between</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="288"/>
+        <source>and</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TabDelegate</name>
+    <message>
+        <location filename="../../src/ui/tabview/TabDelegate.qml" line="82"/>
+        <source>New tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
