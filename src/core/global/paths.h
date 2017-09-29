99a773f6909a34fd159c55f9056cbf4f8419cc2e
Tim Süberkrüb <dev@timsueberkrueb.io>
2017-09-29 16:26:24 +0200

    Add missing license headers

--- src/core/global/paths.h
+++ src/core/global/paths.h
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Liri Browser
+ *
+ * Copyright (C) 2017 Tim Süberkrüb <dev@timsueberkrueb.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PATHS_H
 #define PATHS_H
 
@@ -8,7 +27,6 @@ namespace Paths {
     const QString ConfigLocation = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation) + "/liri-browser/";
     const QString DataLocation = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/liri-browser/";
     const QString SettingsFile = ConfigLocation + "settings.json";
-    const QString ExtensionsLocation = DataLocation + "extensions/";
 }
 
 #endif // PATHS_H
