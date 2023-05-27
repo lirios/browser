add11f501ce269c1dcf26532d708322bf6a7b72f
Liri CI <ci@liri.io>
2023-05-27 01:15:03 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- res/translations/browser.ts
+++ res/translations/browser.ts
@@ -4,34 +4,43 @@
 <context>
     <name>BrowserWindow</name>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="123"/>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="416"/>
         <source>New window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="128"/>
         <source>Private mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="426"/>
         <source>Private Window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="436"/>
         <source>Find in page</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="447"/>
         <source>Downloads</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="457"/>
         <source>Exit fullscreen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="457"/>
         <source>Fullscreen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="466"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
@@ -39,10 +48,12 @@
 <context>
     <name>DownloadItemDelegate</name>
     <message>
+        <location filename="../../src/ui/drawer/content/downloads/DownloadItemDelegate.qml" line="77"/>
         <source>Finished</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/drawer/content/downloads/DownloadItemDelegate.qml" line="79"/>
         <source>Failed</source>
         <translation type="unfinished"></translation>
     </message>
@@ -50,10 +61,12 @@
 <context>
     <name>DrawerDownloadsContent</name>
     <message>
+        <location filename="../../src/ui/drawer/content/downloads/DrawerDownloadsContent.qml" line="33"/>
         <source>Downloads</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/drawer/content/downloads/DrawerDownloadsContent.qml" line="51"/>
         <source>No downloads for this session</source>
         <translation type="unfinished"></translation>
     </message>
@@ -61,6 +74,7 @@
 <context>
     <name>SearchOverlay</name>
     <message>
+        <location filename="../../src/ui/overlay/SearchOverlay.qml" line="56"/>
         <source>Find in page</source>
         <translation type="unfinished"></translation>
     </message>
@@ -68,82 +82,102 @@
 <context>
     <name>SettingsContent</name>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="60"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="101"/>
         <source>Primary</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="117"/>
         <source>Dark theme</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="133"/>
         <source>Incognito</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="150"/>
         <source>Search</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="212"/>
         <source>Custom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="227"/>
         <source>e.g https://example.com/?q=</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="240"/>
         <source>Theme</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="253"/>
         <source>Adapt to website theme colors</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="265"/>
         <source>Light theme</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="277"/>
         <source>Dark theme (always on)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="292"/>
         <source>Dark between</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="328"/>
         <source>and</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="64"/>
         <source>Startup</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="68"/>
         <source>Start new window with</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="70"/>
         <source>New page</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="80"/>
         <source>Continue where you left off</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="92"/>
         <source>New page url</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="351"/>
         <source>Privacy</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/SettingsContent.qml" line="356"/>
         <source>Save cookies</source>
         <translation type="unfinished"></translation>
     </message>
@@ -151,6 +185,8 @@
 <context>
     <name>TabDelegate</name>
     <message id="new-tab">
+        <location filename="../../src/ui/tabview/TabDelegate.qml" line="102"/>
+        <location filename="../../src/ui/window/BrowserWindow.qml" line="127"/>
         <source>New tab</source>
         <translation type="unfinished"></translation>
     </message>
@@ -158,94 +194,117 @@
 <context>
     <name>WebContent</name>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="110"/>
         <source>Back</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="117"/>
         <source>Forward</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="123"/>
         <source>Reload</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="129"/>
         <source>View page source</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="155"/>
         <source>Open in new tab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="163"/>
         <source>Open in new window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Open in  private window</source>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="171"/>
+        <source>Open in private window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="179"/>
         <source>Copy link location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="187"/>
         <source>Download link location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="196"/>
         <source>Cut</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="205"/>
         <source>Copy</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="213"/>
         <source>Paste</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="221"/>
         <source>Select all</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="253"/>
         <source>image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="255"/>
         <source>video</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="257"/>
         <source>audio</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="259"/>
         <source>unknown</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="267"/>
         <source>Open %1 in new tab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="279"/>
         <source>Copy %1 location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="296"/>
         <source>Copy image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="304"/>
         <source>Download %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="327"/>
         <source>This connection is untrusted</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../src/ui/tabview/content/WebContent.qml" line="329"/>
         <source>You are about to securely connect to %1 but we can&apos;t confirm that your connection is secure: %2</source>
         <extracomment>%1 is an URL</extracomment>
         <translation type="unfinished"></translation>
